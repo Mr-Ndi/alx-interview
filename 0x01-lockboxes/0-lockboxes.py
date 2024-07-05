cedd0260f73c8d2f0ac84bd28a7d4ae7581ed833
Mr-Ndi <onlythenotes@gmail.com>
2024-07-05 22:24:56 +0200

    Making a push or a change

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
+"""
+    A module for lockboxes algorithm
+"""
+
+
 def canUnlockAll(boxes):
     """
     Determines if all the boxes can be opened.
 
     Parameters:
-    boxes (list of list of int): A list where each index represents a box and 
-                                 the list at each index contains the keys to other boxes.
+    boxes (list of list of int): A list where each index represents a box and
+    the list at each index contains the keys to other boxes.
 
     Returns:
     bool: True if all boxes can be opened, otherwise False.
