cf117effe9e1f6d6c0aa7f7d06d2a0bf25f392ed
Mr-Ndi <onlythenotes@gmail.com>
2024-07-11 23:42:05 +0200

    second commit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
+"""
+This is the function that solves the Minimum Operations problem.
+"""
 
 
-def minOperations(n: int):
+def minOperations(n: int) -> int:
+    """
+    Finds the minimum number of operations (Copy All and Paste) required
+    to obtain n characters in the text file.
+
+    Args:
+        n (int): The target number of characters.
+
+    Returns:
+        int: The minimum number of operations
+            required, or 0 if it's impossible to achieve.
+    """
     if n <= 1:
         return 0
     # Find the largest divisor of n that is less than n
@@ -9,5 +23,5 @@ def minOperations(n: int):
     while divisor * divisor <= n:
         if n % divisor == 0:
             return divisor + minOperations(n // divisor)
-        divisor += 1
+        divisor += 1  
     return n
