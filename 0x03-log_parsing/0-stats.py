4d11a02e8aded24f5a330232aff64fb2ae74dc43
Mr-Ndi <onlythenotes@gmail.com>
2024-07-19 01:33:55 +0200

    finalised

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,46 @@
 #!/usr/bin/python3
-
-
 import sys
-from collections import defaultdict
-
-# Initialize variables
-total_file_size = 0
-status_codes = defaultdict(int)
+import re
 
 
-# Function to print the statistics
-def print_stats():
-    """Print the current statistics."""
-    print(f"File size: {total_file_size}")
-    for code in sorted(status_codes):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+def print_statistics(total_size, status_codes):
+    print("File size:", total_size)
+    for code in sorted(status_codes.keys()):
+        print("{}: {}".format(code, status_codes[code]))
 
 
-# Read stdin line by line
-line_count = 0
-try:
-    for line in sys.stdin:
-        line_count += 1
-        try:
-            # Parse the input line
-            parts = line.strip().split(" ")
-            if len(parts) != 9:
-                continue
-            ip_address = parts[0]
-            date = parts[3][1:-1]
-            method = parts[5]
-            path = parts[6]
-            protocol = parts[7]
-            status_code = int(parts[8])
-            file_size = int(parts[9])
+def parse_line(line, total_size, status_codes):
+    f = (
+        r'(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] '
+        r'"GET /projects/260 HTTP/1.1" (\d+) (\d+)'
+        )
+    m = re.match(f, line)
+    if m:
+        file_size = int(m.group(4))
+        status_code = m.group(3)
+        total_size += file_size
+        status_codes[status_code] = status_codes.get(status_code, 0) + 1
+        return total_size, status_codes
+    else:
+        return total_size, status_codes
 
-            # Update the metrics
-            total_file_size += file_size
-            status_codes[status_code] += 1
 
-            # Print the statistics every 10 lines or on keyboard interrupt
+def main():
+    total_size = 0
+    status_codes = {}
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            total_size, status_codes = parse_line(line.strip(),
+                                                  total_size, status_codes)
             if line_count % 10 == 0:
-                print_stats()
-        except (ValueError, IndexError):
-            # Skip the line if it doesn't match the expected format
-            continue
-except KeyboardInterrupt:
-    # Print the final statistics on keyboard interrupt
-    print_stats()
+                print_statistics(total_size, status_codes)
+    except KeyboardInterrupt:
+        print_statistics(total_size, status_codes)
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
