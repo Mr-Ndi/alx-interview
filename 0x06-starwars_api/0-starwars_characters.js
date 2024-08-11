40a7a6d7b0ca3a612ac8954115cc36554456f095
Mr-Ndi <onlythenotes@gmail.com>
2024-08-11 19:48:18 +0200

    cleear version

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,40 +1,35 @@
 #!/usr/bin/node
-/*
-Star Wars Characters
-*/
-
-const arg1 = process.argv[2];
 const request = require('request');
 
-async function makeRequest (url) {
-  const options = {
-    url,
-    method: 'GET',
-    json: true
-  };
-
+async function makeRequest(url) {
   return new Promise((resolve, reject) => {
-    request(options, (error, response, body) => {
+    request(url, { json: true }, (error, response, body) => {
       if (error) {
-        reject(error);
-      } else {
-        resolve(body);
+        return reject(error);
       }
+      resolve(body);
     });
   });
 }
 
-async function getData (url) {
+async function getStarWarsCharacters(movieId) {
   try {
-    const body = await makeRequest(url);
-    const characters = body.characters;
-    for (const peopleUrl of characters) {
-      const character = await makeRequest(peopleUrl);
-      console.log(character.name);
+    const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+    const filmData = await makeRequest(filmUrl);
+    const characterUrls = filmData.characters;
+
+    for (const url of characterUrls) {
+      const characterData = await makeRequest(url);
+      console.log(characterData.name);
     }
   } catch (error) {
     console.error(error);
   }
 }
-const url = `https://swapi-api.alx-tools.com/api/films/${arg1}`;
-getData(url);
+
+const movieId = process.argv[2];
+if (movieId) {
+  getStarWarsCharacters(movieId);
+} else {
+  console.error('Please provide a Movie ID as an argument.');
+}
