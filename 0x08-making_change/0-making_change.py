ff91616aa0e4c15c43231a3aa963d58a57a21e7e
Mr-Ndi <onlythenotes@gmail.com>
2024-08-22 22:07:01 +0200

    final one

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""
-A module that contains the function to solves the coin issue
-"""
+
+""" Contains makeChange function"""
 
 
 def makeChange(coins, total):
     """
-    A function to determine the fewest number of coins
-    needed to meet a given amount total. through the following phase:
-    - Initialize a list to store the minimum coins
-    - needed for each amount up to total
-    - Base case: 0 coins are needed to make the total of 0
-    - Iterate over each coin
-    - Update the mincoin array for each amount from coin to total
-    - If mincoin[total] is still infinity, it means total cannot be met
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
     """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-    mincoin = [float('inf')] * (total + 1)
-    mincoin[0] = 0
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        for amount in range(coin, total + 1):
-            mincoin[amount] = min(mincoin[amount], mincoin[amount - coin] + 1)
-    return mincoin[total] if mincoin[total] != float('inf') else -1
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
